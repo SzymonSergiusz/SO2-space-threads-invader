b7f3823216e112bbb572c0a8e684889bd9127f3e
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2024-05-12 03:46:13 +0200

    +new todos

--- TODO.py
+++ TODO.py
@@ -1,10 +1,13 @@
 """
 
--- trzeba dodać quit na wątek?
+-- trzeba dodać quit na wątek? poprawić aby quit działał
 -- jakieś elementy w tle aby było dynamiczniej --są, nie wiem czy jest dynamiczniej
 -- różne muzyki do bossów
 -- jeszcze jeden boss, może trochę inny niż reszta (ta ryba mogłaby szarżować do przodu albo wysyłać
     małe ryby które by szarżowały do przodu i trzeba byłoby je zestrzeliwywać)
 -- wymienić sprite'y przeciwników
+//////
+
+-- może jakieś ui? tzn opcje jakieś typu głośność muzyki
 """
 
